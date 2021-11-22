0aba06f067a56403be11a92397553372be41be17
yifan li <ucapiaf@ucl.ac.uk>
2021-11-22 17:25:52 +0000

    use the pandas library

--- glaciers.py
+++ glaciers.py
@@ -1,3 +1,14 @@
+import pandas as pd
+# The Python Data Analysis Library or pandas is a tool based on NumPy 
+# that was created to solve data analysis tasks. pandas incorporates a 
+# large number of libraries and some standard data models to provide the 
+# tools needed to efficiently manipulate large data sets. pandas provides 
+# a number of functions and methods that allow us to quickly and easily 
+# manipulate functions and methods that enable us to work with data quickly
+#  and easily. You will soon discover that it is one of the key factors that 
+# make Python a powerful and efficient environment for data analysis.
+
+Translated with www.DeepL.com/Translator (free version)
 class Glacier:
     def __init__(self, glacier_id, name, unit, lat, lon, code):
         raise NotImplementedError
