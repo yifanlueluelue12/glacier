695b3179240c33e2eab9396d0b6ce0e99ec08e53
yifan li <ucapiaf@ucl.ac.uk>
2021-11-22 21:44:22 +0000

    finish the glacier classfication function

--- glaciers.py
+++ glaciers.py
@@ -1,3 +1,4 @@
+from utils import haversine_distance
 import pandas as pd
 # The Python Data Analysis Library or pandas is a tool based on NumPy 
 # that was created to solve data analysis tasks. pandas incorporates a 
@@ -7,12 +8,35 @@ import pandas as pd
 # manipulate functions and methods that enable us to work with data quickly
 #  and easily. You will soon discover that it is one of the key factors that 
 # make Python a powerful and efficient environment for data analysis.
-
-Translated with www.DeepL.com/Translator (free version)
+import matplotlib.pyplot as plt
+import re
+import datetime
 class Glacier:
     def __init__(self, glacier_id, name, unit, lat, lon, code):
-        raise NotImplementedError
+        self.year=[] # define the year as the matrix
+        self.lon=lon
+        if float(self.lon)>=-180: # distinguish if longitude value meets the requirement
+             print (' longitude value satisfies the minimum limit')
+        elif float(self.lon)<=180:
+             print ('longitude value satisfies the maximum limit')
+        else:
+             print ('the value of input longitude is an error')
+        self.mass_balance=[]
+        self.glacier_id = glacier_id
+        if glacier_id.isdigit()== True and len(self.glacier_id)==5:
+          print ('input a right glacier_id')
+        else :
+          print ('input an error glacier_id') # distinguish if glacier_id value meets the requirement
+        self.lat=lat
+        if float(self.lat)>=-90:
+             print ('longitude value satisfies the minimum limit')
+        elif float(self.lat)<=90:
+             print ('longitude value satisfies the maximum limit')
+        else:
+             print ('the value of input longitude is an error')
+        self.code =code
 
+     
     def add_mass_balance_measurement(self, year, mass_balance):
         raise NotImplementedError
 
