9ca3a4573a9b20b35319a0f60c7a3a8c4b7f8e85
yifan li <ucapiaf@ucl.ac.uk>
2021-11-22 20:26:25 +0000

    use the function to represent the distance

--- utils.py
+++ utils.py
@@ -1,6 +1,14 @@
+from math import radians,asin, cos, sin, sqrt
 def haversine_distance(lat1, lon1, lat2, lon2):
     """Return the distance in km between two points around the Earth.
 
     Latitude and longitude for each point are given in degrees.
     """
-    raise NotImplementedError
+    lon1,lat1,lon2,lat2 = map (radians,[lon1,lat1,lon2,lat2]) #transfer the angle to the curvature
+    latd = lat2 - lat1
+    lond = lon2 - lon1
+    t= cos(lat2)*cos(lat1)* sin(lond/2)**2 + sin(latd/2)**2
+    z = asin(sqrt(t))*2
+    R = 6371 # Radius of earth in kilometers
+   
+    return z * R
