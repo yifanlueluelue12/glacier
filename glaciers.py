caa38205bc26b18c4890380a14cae3f29d370e4a
yifan li <ucapiaf@ucl.ac.uk>
2021-11-24 21:27:27 +0000

    change from the visual studio code to Pycharm. Try to search the website and find the solutions to read the csv file with the help pf standard library in Python

--- glaciers.py
+++ glaciers.py
@@ -1,39 +1,43 @@
 from utils import haversine_distance
-
-# The Python Data Analysis Library or pandas is a tool based on NumPy 
-# that was created to solve data analysis tasks. pandas incorporates a 
-# large number of libraries and some standard data models to provide the 
-# tools needed to efficiently manipulate large data sets. pandas provides 
-# a number of functions and methods that allow us to quickly and easily 
+# The Python Data Analysis Library or pandas is a tool based on NumPy
+# that was created to solve data analysis tasks. pandas incorporates a
+# large number of libraries and some standard data models to provide the
+# tools needed to efficiently manipulate large data sets. pandas provides
+# a number of functions and methods that allow us to quickly and easily
 # manipulate functions and methods that enable us to work with data quickly
-#  and easily. You will soon discover that it is one of the key factors that 
+#  and easily. You will soon discover that it is one of the key factors that
 # make Python a powerful and efficient environment for data analysis.
 import matplotlib.pyplot as plt
 from datetime import datetime
+from pathlib import Path
+
+
 class Glacier:
     def __init__(self, glacier_id, name, unit, lat, lon, code):
-        self.year=[] # define the year as the matrix
-        self.lon=lon
-        if float(self.lon)>=-180: # distinguish if longitude value meets the requirement
-             print (' longitude value satisfies the minimum limit')
-        elif float(self.lon)<=180:
-             print ('longitude value satisfies the maximum limit')
+        self.year = []  # define the year as the matrix
+        self.lon = lon
+        if float(self.lon) >= -180:  # distinguish if longitude value meets the requirement
+            print(' longitude value satisfies the minimum limit')
+        elif float(self.lon) <= 180:
+            print('longitude value satisfies the maximum limit')
         else:
-             print ('the value of input longitude is an error')
-        self.mass_balance=[]
+            raise ValueError
+            print('the value of input longitude is an error')
+        self.mass_balance = []
         self.glacier_id = glacier_id
-        if glacier_id.isdigit()== True and len(self.glacier_id)==5:
-          print ('input a right glacier_id')
-        else :
-          print ('input an error glacier_id') # distinguish if glacier_id value meets the requirement
-        self.lat=lat
-        if float(self.lat)>=-90:
-             print ('longitude value satisfies the minimum limit')
-        elif float(self.lat)<=90:
-             print ('longitude value satisfies the maximum limit')
+        if glacier_id.isdigit() == True and len(self.glacier_id) == 5:
+            print('input a right glacier_id')
         else:
-             print ('the value of input longitude is an error')
-        self.code =code
+            print('input an error glacier_id')  # distinguish if glacier_id value meets the requirement
+        self.lat = lat
+        if float(self.lat) >= -90:
+            print('longitude value satisfies the minimum limit')
+        elif float(self.lat) <= 90:
+            print('longitude value satisfies the maximum limit')
+        else:
+            raise ValueError
+            print('the value of input longitude is an error')
+        self.code = code
 
     def add_mass_balance_measurement(self, year, mass_balance):
         currentDateTime = datetime.datetime.now()
@@ -52,16 +56,46 @@ class Glacier:
         plt.savefig('output_path/.png')
         plt.show()
 
+
 class GlacierCollection:
+
     def __init__(self, file_path):
         self.file_path = file_path
-        self.glacierscollection = glacierscollection
-        glaciersdata = pd.read_csv(self.file_path)
-        print(pd_reader)
-        dataform = list(zip(glaciersdata['WGMS_ID'],glaciersdata['NAME'],glaciersdata['POLITICAL_UNIT'],
-                   glaciersdata['LATITUDE'],glaciersdata['LONGITUDE'],glaciersdata['PRIM_CLASSIFIC'],
-                   glaciersdata['From'],glaciersdata['FRONTAL_CHARS']))
-        
+        file_path = Path("../sheet-A.csv")
+        collection = GlacierCollection(file_path)
+        dataform = list(zip(glaciersdata['WGMS_ID'], glaciersdata['NAME'], glaciersdata['POLITICAL_UNIT'],
+                            glaciersdata['LATITUDE'], glaciersdata['LONGITUDE'], glaciersdata['PRIM_CLASSIFIC'],
+                            glaciersdata['From'], glaciersdata['FRONTAL_CHARS']))
+        self.glacier_collection = [Glacier(row[0], row[1], row[2], row[3], row[4], row[5])]
+
+
+    def read_mass_balance_data(self, file_path):
+        mass_balance = pd.read_csv(file_path)
+
+    def find_nearest(self, lat, lon, n=5):
+        """Get the n glaciers closest to the given coordinates."""
+        distance = []
+        for glacier in self.glacier_collection:
+            D = haversine_distance(lat, lon, glacier.lat, glacier.lon)
+            distance.append(D)
+        print(distance.index(min(distance)))
+
+    def filter_by_code(self, code_pattern):
+        """Return the names of glaciers whose codes match the given pattern."""
+
+        raise NotImplementedError
+
+    def sort_by_latest_mass_balance(self, n=5, reverse=True):
+        """Return the N glaciers with the highest area accumulated in the last measurement."""
+
+        raise NotImplementedError
+
+    def summary(self):
+        raise NotImplementedError
+
+    def plot_extremes(self, output_path):
+        raise NotImplementedError
+
     
 
 
