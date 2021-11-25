d2504e8b812f211373b8f93f48726bb80602323c
yifan li <ucapiaf@ucl.ac.uk>
2021-11-25 18:15:32 +0000

    finish the GlacierCollection function with the help after reading the csv file and extract the data into the matrix for the next application.

--- glaciers.py
+++ glaciers.py
@@ -1,12 +1,4 @@
 from utils import haversine_distance
-# The Python Data Analysis Library or pandas is a tool based on NumPy
-# that was created to solve data analysis tasks. pandas incorporates a
-# large number of libraries and some standard data models to provide the
-# tools needed to efficiently manipulate large data sets. pandas provides
-# a number of functions and methods that allow us to quickly and easily
-# manipulate functions and methods that enable us to work with data quickly
-#  and easily. You will soon discover that it is one of the key factors that
-# make Python a powerful and efficient environment for data analysis.
 import matplotlib.pyplot as plt
 from datetime import datetime
 from pathlib import Path
@@ -16,14 +8,16 @@ class Glacier:
     def __init__(self, glacier_id, name, unit, lat, lon, code):
         self.year = []  # define the year as the matrix
         self.lon = lon
+        self.name = name
         if float(self.lon) >= -180:  # distinguish if longitude value meets the requirement
-            print(' longitude value satisfies the minimum limit')
+            print(" longitude value satisfies the minimum limit")
         elif float(self.lon) <= 180:
-            print('longitude value satisfies the maximum limit')
+            print("longitude value satisfies the maximum limit")
         else:
             raise ValueError
             print('the value of input longitude is an error')
         self.mass_balance = []
+        self.unit = unit
         self.glacier_id = glacier_id
         if glacier_id.isdigit() == True and len(self.glacier_id) == 5:
             print('input a right glacier_id')
@@ -40,14 +34,15 @@ class Glacier:
         self.code = code
 
     def add_mass_balance_measurement(self, year, mass_balance):
+        self.mass_balance =mass_balance
         currentDateTime = datetime.datetime.now()
         date = currentDateTime.date()
         currentyear = date.strftime("%Y")
-        while int(year) <= currentyear and int(year) >= 1963 and year.isdigit() == Ture:
+        if int(year) <= currentyear and int(year) >= 1963 and year.isdigit() == True:
             print('the input year is the right value')
         else:
             raise ValueError(
-                "the input year in the file is a wrong number of because the detection year comes from 1963 to currentyear")
+                'the input year in the file is a wrong number of because the detection year comes from 1963 to currentyear')
 
     def plot_mass_balance(self, output_path):
         plt.plot(self.year, self.mass_balance)
@@ -58,32 +53,34 @@ class Glacier:
 
 
 class GlacierCollection:
-
     def __init__(self, file_path):
         self.file_path = file_path
-        file_path = Path("../sheet-A.csv")
+        file_path = Path("sheet-A.csv")
         collection = GlacierCollection(file_path)
-        dataform = list(zip(glaciersdata['WGMS_ID'], glaciersdata['NAME'], glaciersdata['POLITICAL_UNIT'],
-                            glaciersdata['LATITUDE'], glaciersdata['LONGITUDE'], glaciersdata['PRIM_CLASSIFIC'],
-                            glaciersdata['From'], glaciersdata['FRONTAL_CHARS']))
-        self.glacier_collection = [Glacier(row[0], row[1], row[2], row[3], row[4], row[5])]
+        self.collection = collection
+        glacierdata_row = zip(collection['WGMS_ID'], collection['NAME'], collection['POLITICAL_UNIT']
+                       , collection['LATITUDE'], collection['LONGITUDE'],collection['PRIM_CLASSIFIC'] )
 
+        self.glacier = [Glacier(row[0], row[1], row[2], row[3], row[4], row[5]) for row in glacierdata_row]
 
     def read_mass_balance_data(self, file_path):
-        mass_balance = pd.read_csv(file_path)
+        self.file_path = file_path
+
+
 
     def find_nearest(self, lat, lon, n=5):
-        """Get the n glaciers closest to the given coordinates."""
-        distance = []
-        for glacier in self.glacier_collection:
-            D = haversine_distance(lat, lon, glacier.lat, glacier.lon)
-            distance.append(D)
-        print(distance.index(min(distance)))
+            """Get the n glaciers closest to the given coordinates."""
+            distance = []
+            for glacier in self.glacier:
+                D = haversine_distance(lat, lon, glacier.lat, glacier.lon)
+                distance.append(D)
+            print(distance.index(min(distance)))
 
     def filter_by_code(self, code_pattern):
         """Return the names of glaciers whose codes match the given pattern."""
 
         raise NotImplementedError
+
 
     def sort_by_latest_mass_balance(self, n=5, reverse=True):
         """Return the N glaciers with the highest area accumulated in the last measurement."""
@@ -95,8 +92,4 @@ class GlacierCollection:
 
     def plot_extremes(self, output_path):
         raise NotImplementedError
-
-    
-
-
 
